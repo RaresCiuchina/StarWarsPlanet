e9517d56f0cae627a41a89105f17860f2e68f436
RARES CIUCHINA <raresciuchina01@gmail.com>
2022-05-23 18:28:41 +0200

    Primer Commit

--- src/main/java/Ejercicio6.java
+++ src/main/java/Ejercicio6.java
@@ -1,9 +1,75 @@
+import java.util.Arrays;
+
 public class Ejercicio6 {
 
     public Planets planets;
 
     public Ejercicio6() {
         planets = new Planets(ObtenerPlanetsRequest.Companion.get().getResults());
+    }
+
+    public Planet[] cuantosPlanetasHay() {
+
+        int cont = 0;
+        Planet[] array = planets.obtenerTodos();
+        for (Planet planeta : array) {
+            if (!planeta.getName().equals("")) {
+                array[cont] = planeta;
+                cont++;
+            }
+        }
+        return getArraySinNulos(array, cont);
+    }
+
+    public Planet[] getArraySinNulos(Planet[] array, int nulos) {
+        return Arrays.copyOfRange(array, 0, nulos);
+    }
+
+
+    // TODO Haz la función en la clase Ejercicio6:
+    //  obtenerMasPobladosQue(int poblacion)
+    //  nos devuelve una lista de aquellos planetas que tienen más población
+    //  que el integer pasado por parámetro
+
+    public Planet[] obtenerMasPobladosQue(int poblacion) {
+        Planet[] fuera = new Planet[planets.obtenerTodos().length];
+        int i = 0;
+
+        for (Planet planeta : planets.obtenerTodos()) {
+            if (planeta.getPopulation().equals("unknown")) {
+                break;
+            }
+                int a = Integer.parseInt(planeta.getPopulation());
+                if (a > poblacion) {
+                    fuera[i] = planeta;
+                    i++;
+                }
+
+            }
+
+            return getArraySinNulos(fuera, i);
+        }
+
+    //TODO Haz la función en la clase Ejercicio6:
+    // obtenerPlanetasConTerreno(String terreno)
+    //  nos devuelve una lista de aquellos planetas
+    //  que tienen terrenos que se llamen como el string.
+
+    public Planet[] obtenerPlanetasConTerreno(String terreno){
+        Planet[] fuera = new Planet[planets.obtenerTodos().length];
+        int i = 0;
+
+        for(Planet planeta : fuera){
+            if (planeta.getPopulation().equals("unknown")) {
+                break;
+            }
+            if (planeta.getTerrain().equals(terreno)){
+                fuera[i]=planeta;
+                i++;
+            }
+        }
+
+        return getArraySinNulos(fuera,i);
     }
 
 
--- src/test/java/PersonajeTest.java
+++ src/test/java/PersonajeTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PersonajeTest {
@@ -12,19 +13,50 @@ public class PersonajeTest {
         // tip: ej.planets.obtenerTodos()
         // TODO comprueba que tu función devuelve el número correcto de elementos
         // tip: es 10
+        Assertions.assertEquals(10,
+                ej.cuantosPlanetasHay().length);
     }
 
-    // TODO Haz la función en la clase Ejercicio6: obtenerMasPobladosQue(int poblacion)
-    //  nos devuelve una lista de aquellos planetas que tienen más población que el integer pasado por parámetro
-    // TODO Haz todos los test que sean necesarios.
-    // tip: Si el planeta tiene una población indeterminada, no debe mostrarse
+
+        // TODO Haz la función en la clase Ejercicio6:
+        //  obtenerMasPobladosQue(int poblacion)
+        //  nos devuelve una lista de aquellos planetas que
+        //  tienen más población que el integer pasado por parámetro
+        // TODO Haz todos los test que sean necesarios.
+        // tip: Si el planeta tiene una población indeterminada, no debe mostrarse
+    @Test
+    public void PruebaMasPobladoQue0() {
+        Assertions.assertEquals( 3,ej.obtenerMasPobladosQue(0).length);
+    }
+    @Test
+    public void PruebaMasPobladoQuemenos1() {
+        Assertions.assertEquals( 3,ej.obtenerMasPobladosQue(-1).length);
+    }
+    @Test
+    public void PruebaMasPobladoQue1000() {
+        Assertions.assertEquals( 2,ej.obtenerMasPobladosQue(1000).length);
+    }
+    @Test
+    public void PruebaMasPobladoQue100000() {
+        Assertions.assertEquals( 2,ej.obtenerMasPobladosQue(100000).length);
+    }
+    @Test
+    public void PruebaMasPobladoQue1000000() {
+        Assertions.assertEquals( 1,ej.obtenerMasPobladosQue(1000000).length);
+    }
+
+    @Test
+    public void PruebaMasPobladoQue10000000() {
+        Assertions.assertEquals( 1,ej.obtenerMasPobladosQue(10000000).length);
+    }
+
 
 
     // TODO Haz la función en la clase Ejercicio6: obtenerPlanetasConTerreno(String terreno)
     //  nos devuelve una lista de aquellos planetas que tienen terrenos que se llamen como el string.
     // tip: Algunos terrenos disponibles: forests, mountains, hills, etc.
     // TODO Haz todos los test que sean necesarios.
-    // tip: Si el planeta tiene una población indeterminada, no debe mostrarse
+    // tip: Si el planeta tiene un terreno indeterminada, no debe mostrarse
 
 
     // TODO Haz la función en la clase Ejercicio6: obtenerPlanetasResidentes(int residentes, String nombre)
